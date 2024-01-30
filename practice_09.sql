63e7a2547d070d1afafad1612a88b6a0bac2a44d
Nguyengiang1209 <156086209+Nguyengiang1209@users.noreply.github.com>
2024-01-31 02:04:15 +0700

    Update practice_09.sql

--- practice_09.sql
+++ practice_09.sql
@@ -22,3 +22,28 @@ set contactlastname = (Right(contactfullname, length(contactfullname)-position('
 where contactlastname is Null;
 UPDATE public.sales_dataset_rfm_prj
 SET contactlastname= CONCAT(UPPER(LEFT(contactlastname,1)),LOWER(RIGHT(contactlastname,LENGTH(contactlastname)-1)))
+--4
+alter table public.sales_dataset_rfm_prj
+add column QTR_ID numeric,
+add column MONTH_ID numeric,
+add column YEAR_ID numeric;
+
+update public.sales_dataset_rfm_prj
+set month_id = EXTRACT(MONTH FROM orderdate) 
+where month_id is null
+
+update public.sales_dataset_rfm_prj
+set year_id = EXTRACT(year FROM orderdate) 
+where year_id is null
+
+update public.sales_dataset_rfm_prj
+set QTR_ID =(case
+when month_id between 1 and 3 then 1
+when month_id between 4 and 6 then 2
+when month_id between 7 and 9 then 3
+else '4'
+end)
+where QTR_ID is null
+--5
+
+
